8a873fd419b15a1e1814a52c775f91caea327623
tolyan <tolyan@SCTB28>
2015-09-29 08:24:34 +0300

    segfault

--- backend_windows.go
+++ backend_windows.go
@@ -12,6 +12,7 @@ import (
 	"errors"
 	"fmt"
 	"golang.org/x/sys/windows/registry"
+	"log"
 	"strings"
 	"unsafe"
 )
@@ -34,6 +35,7 @@ func is_INVALID_HANDLE_VALUE(p interface{}) bool {
 
 func getNativeName(DeviceInfoSet C.HDEVINFO, DeviceInfoData C.PSP_DEVINFO_DATA) (string, error) {
 	key := C.SetupDiOpenDevRegKey(DeviceInfoSet, DeviceInfoData, C.DICS_FLAG_GLOBAL, 0, C.DIREG_DEV, C.KEY_READ)
+	defer C.RegCloseKey(key)
 	if is_INVALID_HANDLE_VALUE(key) {
 		return "", errors.New(fmt.Sprintf("Reg error: %d", int(C.GetLastError())))
 	}
@@ -59,6 +61,72 @@ func getNativeName(DeviceInfoSet C.HDEVINFO, DeviceInfoData C.PSP_DEVINFO_DATA) 
 	return "", errors.New("Empty response")
 }
 
+func getDeviceRegistryProperty(DeviceInfoSet C.HDEVINFO, DeviceInfoData C.PSP_DEVINFO_DATA, property C.DWORD) (interface{}, error) {
+	var dataType C.DWORD = 0
+	var dataSize C.DWORD = 0
+
+	C.SetupDiGetDeviceRegistryProperty(DeviceInfoSet, DeviceInfoData, property, &dataType, (*C.BYTE)(nil), (C.DWORD)(0), &dataSize)
+	data := make([]C.BYTE, dataSize)
+
+	if C.SetupDiGetDeviceRegistryProperty(DeviceInfoSet, DeviceInfoData, property, (*C.DWORD)(nil), &data[0], dataSize, (*C.DWORD)(nil)) == C.TRUE {
+		switch dataType {
+		case C.REG_EXPAND_SZ, C.REG_SZ:
+			if dataSize > 0 {
+				return C.GoString((*C.char)(unsafe.Pointer(&data[0]))), nil
+			}
+		case C.REG_MULTI_SZ:
+			if dataSize > 0 {
+				var res []string
+				i := 0
+				for {
+					s := C.GoString((*C.char)(unsafe.Pointer(&data[i])))
+					if len(s) == 0 {
+						break
+					} else {
+						i += len(s)
+						res = append(res, s)
+					}
+				}
+				return res, nil
+			}
+		case C.REG_DWORD_BIG_ENDIAN, C.REG_DWORD:
+			var t C.int
+			if uintptr(dataSize) != unsafe.Sizeof(t) {
+				panic("Reg incorrect result")
+			}
+			return int(*(*C.int)(unsafe.Pointer(&data[0]))), nil
+		}
+	}
+
+	return nil, errors.New("Failed to get data from regestry")
+}
+
+func getNativeDriver(service string) (string, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SYSTEM\CurrentControlSet\services\`+service, registry.QUERY_VALUE)
+	if err != nil {
+		return "", err
+	}
+	defer k.Close()
+
+	if v, _, err := k.GetStringValue("ImagePath"); err == nil {
+		return v, nil
+	} else {
+		return "", err
+	}
+}
+
+func result2string(v interface{}, e error) (s string, err error) {
+	if e != nil {
+		return "", e
+	} else {
+		if ss, ok := v.(string); ok == false {
+			return "", errors.New("Not a string value")
+		} else {
+			return ss, nil
+		}
+	}
+}
+
 func enumerate() ([]DeviceDescription, error) {
 	//log.Print("Calling windows backend")
 
@@ -67,6 +135,8 @@ func enumerate() ([]DeviceDescription, error) {
 		return nil, err
 	}
 	defer k.Close()
+
+	var result []DeviceDescription
 
 	for i := 0; i < len(guidArray); i++ {
 		DeviceInfoSet := C.SetupDiGetClassDevs(&guidArray[i], (*C.CHAR)(nil), (*C.struct_HWND__)(nil), C.DIGCF_PRESENT)
@@ -80,8 +150,41 @@ func enumerate() ([]DeviceDescription, error) {
 
 		var DeviceIndex C.DWORD = 0
 		var DeviceInfoData C.SP_DEVINFO_DATA
+		DeviceInfoData.cbSize = C.DWORD(unsafe.Sizeof(DeviceInfoData))
 		for C.SetupDiEnumDeviceInfo(DeviceInfoSet, DeviceIndex, &DeviceInfoData) == C.TRUE {
+			name, err := getNativeName(DeviceInfoSet, &DeviceInfoData)
+			if err != nil || len(name) == 0 || strings.Contains(name, "LPT") {
+				continue
+			}
 
+			var dev DeviceDescription
+			dev.Name = name
+			if dev.Bus, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_ENUMERATOR_NAME)); err != nil {
+				log.Printf("Error get bus of device %s: %s", name, err.Error())
+			}
+			if dev.Description, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_DEVICEDESC)); err != nil {
+				log.Printf("Error get Description of device %s: %s", name, err.Error())
+			}
+			if dev.FriendlyName, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_FRIENDLYNAME)); err != nil {
+				log.Printf("Error get FriendlyName of device %s: %s", name, err.Error())
+			}
+			if dev.HardwareID, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_HARDWAREID)); err != nil {
+				log.Printf("Error get HardwareID of device %s: %s", name, err.Error())
+			}
+			if dev.LocationInfo, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_LOCATION_INFORMATION)); err != nil {
+				log.Printf("Error get LocationInfo of device %s: %s", name, err.Error())
+			}
+			if dev.Manufacturer, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_MFG)); err != nil {
+				log.Printf("Error get Manufacturer of device %s: %s", name, err.Error())
+			}
+			if dev.SubSystem, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_CLASS)); err != nil {
+				log.Printf("Error get SubSystem of device %s: %s", name, err.Error())
+			}
+			if dev.Service, err = result2string(getDeviceRegistryProperty(DeviceInfoSet, &DeviceInfoData, C.SPDRP_SERVICE)); err != nil {
+				log.Printf("Error get Service of device %s: %s", name, err.Error())
+			}
+
+			result = append(result, dev)
 		}
 	}
 
--- serialdeviceenumeratorgo_windows_test.go
+++ serialdeviceenumeratorgo_windows_test.go
@@ -1,11 +1,23 @@
 package serialdeviceenumeratorgo
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestGUIDLen(t *testing.T) {
 	if len(guidArray) != 3 {
 		t.Error("en(guidArray) != 3")
+	}
+}
+
+func TestRegAccess(t *testing.T) {
+	s, err := getNativeDriver("FTDIBUS")
+	if err != nil {
+		t.Error(fmt.Sprintf("regestr querry error: %s", err.Error()))
+	}
+	if !strings.Contains(s, "bus.sys") {
+		t.Error(fmt.Sprintf("invalid result of regestry: %s", s))
 	}
 }
